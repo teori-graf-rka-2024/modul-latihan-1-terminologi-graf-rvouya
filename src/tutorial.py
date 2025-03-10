9c179852102b8a68393694f94d6f998026894e23
kaykay <kaylarizaa@gmail.com>
2025-03-10 17:44:34 +0700

    KNPP GACENTANG

--- src/tutorial.py
+++ src/tutorial.py
@@ -4,16 +4,21 @@ import graph as g
 # edge_list = [(1, 2), (2,3), (3,5), (1,4), (1,5)]
 
 # tree
-edge_list = [
-    (0, 1), (0, 2),
-    (1, 3), (1, 4),
-    (2, 5), (2, 6),
-    (4, 7), (4, 8)
-]
+# edge_list = [
+#     (0, 1), (0, 2),
+#     (1, 3), (1, 4),
+#     (2, 5), (2, 6),
+#     (4, 7), (4, 8)
+# ]
 
-G = g.create_graph(edge_list)
-print(g.get_degree(G, 1))
+# edges = [(1, 2), (2, 3), (3, 4), (4, 5), (5, 1), (2, 5)]
+edges = [(1, 2), (2, 3), (3, 4)]
+
+G = g.create_graph(edges)
+print(len(G.nodes))
+print(len(G.edges))
+print(g.get_degree(G, 2))
 print(g.dfs_traversal(G, 1))
 print(g.bfs_traversal(G, 1))
-print(g.find_shortest_path(G, 0, 6)) # gimana caranya supaya tidak ketemu? kan edge_list pasti ada pasangan dong??
+print(g.find_shortest_path(G, 1, 4)) # gimana caranya supaya tidak ketemu? kan edge_list pasti ada pasangan dong??
 g.visualize_graph(G)
